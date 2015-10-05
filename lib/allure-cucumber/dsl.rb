d0ab8a6f8520a7cb19b7bb13e0c8dd1f40973720
Vitalii Grygoruk <vitaliy.grigoruk@gmail.com>
2015-10-05 10:25:57 +0200

    Added support for file attachments to scenario via AllureCucumber::DSL.attach_file

--- lib/allure-cucumber/dsl.rb
+++ lib/allure-cucumber/dsl.rb
@@ -1,9 +1,11 @@
 module AllureCucumber
   module DSL
 
-    def attach_file(title, file)
+    def attach_file(title, file, attach_to_step=true)
       @tracker = AllureCucumber::FeatureTracker.tracker
-      AllureRubyAdaptorApi::Builder.add_attachment(@tracker.feature_name, @tracker.scenario_name, :step => @tracker.step_name, :file => file, :title => title)
+      options = {:file => file, :title => title}
+      options.merge!(:step => @tracker.step_name) if attach_to_step
+      AllureRubyAdaptorApi::Builder.add_attachment(@tracker.feature_name, @tracker.scenario_name, options)
     end
 
   end
