allure-framework/allure-cucumber

commit 05d21398fdc6d777876cb222310aafe7257f33ac

fix for duplicate steps (via #65)

allure-cucumber.gemspec
@@ -17,7 +17,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'cucumber' , '>= 2.0.0'
-  spec.add_dependency 'allure-ruby-adaptor-api'
+  spec.add_dependency 'allure-ruby-adaptor-api', '>= 0.7.1'
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"

lib/allure-cucumber/version.rb
@@ -1,5 +1,5 @@
-module AllureCucumber  
-  module Version 
-    STRING = '0.6.0'
+module AllureCucumber
+  module Version
+    STRING = '0.6.1'
   end
 end

Rakefile
@@ -1,1 +1,3 @@
 require "bundler/gem_tasks"
+
+task :default => :build

lib/allure-cucumber/feature_tracker.rb
@@ -2,7 +2,7 @@ module AllureCucumber
   
   class FeatureTracker
 
-    attr_accessor :feature_name, :scenario_name, :step_name
+    attr_accessor :feature_name, :scenario_name, :step_name, :step_id
     @@tracker = nil
 
     def self.create

lib/allure-cucumber/formatter.rb
@@ -109,7 +109,10 @@ module AllureCucumber
     def before_test_step(test_step)
       if !TEST_HOOK_NAMES_TO_IGNORE.include?(test_step.name) 
         if @tracker.scenario_name
-          @tracker.step_name = test_step.name
+          step_location =  test_step.location.lines.first.to_s
+          step_name = test_step.name
+          @tracker.step_id = "#{@tracker.feature_name}-#{@tracker.scenario_name}-#{step_name}-#{step_location}"
+          @tracker.step_name = step_name
           start_step
         else
           @deferred_before_test_steps << {:step => test_step, :timestamp => Time.now}
@@ -216,7 +219,10 @@ module AllureCucumber
 
     def post_deferred_steps
       @deferred_before_test_steps.size.times do |index|
-        @tracker.step_name = @deferred_before_test_steps[index][:step].name 
+        step_location =  @deferred_before_test_steps[index][:step].location.lines.first.to_s
+        step_name = @deferred_before_test_steps[index][:step].name
+        @tracker.step_id = "#{@tracker.feature_name}-#{@tracker.scenario_name}-#{step_name}-#{step_location}"
+        @tracker.step_name = step_name
         start_step
         multiline_arg = @deferred_before_test_steps[index][:multiline_arg]
         attach_multiline_arg_to_file(multiline_arg) if multiline_arg
@@ -240,15 +246,14 @@ module AllureCucumber
         @before_hook_exception = nil
       end
     end
-    
-    def start_step(step_name = @tracker.step_name)
-      AllureRubyAdaptorApi::Builder.start_step(@tracker.feature_name, @tracker.scenario_name, step_name) 
+
+    def start_step(step_name = @tracker.step_name, step_id = @tracker.step_id)
+      AllureRubyAdaptorApi::Builder.start_step(@tracker.feature_name, @tracker.scenario_name, step_name, step_id)
     end
 
-    def stop_step(status, step_name = @tracker.step_name)
-      AllureRubyAdaptorApi::Builder.stop_step(@tracker.feature_name, @tracker.scenario_name, step_name, status) 
+    def stop_step(status, step_name = @tracker.step_name, step_id = @tracker.step_id)
+      AllureRubyAdaptorApi::Builder.stop_step(@tracker.feature_name, @tracker.scenario_name, step_name, step_id, status)
     end
-    
-  end  
+
+  end
 end
-

commit c0e10a252c486064d338a96a53b95bc567d303be

fix regression introduced in #65 (fixes #67, fixes #68, fixes #69, via #70)

allure-cucumber.gemspec
@@ -17,7 +17,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'cucumber' , '>= 2.0.0'
-  spec.add_dependency 'allure-ruby-adaptor-api', '>= 0.7.1'
+  spec.add_dependency 'allure-ruby-adaptor-api', '>= 0.7.2'
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"

lib/allure-cucumber/dsl.rb
@@ -4,7 +4,7 @@ module AllureCucumber
     def attach_file(title, file, attach_to_step=true)
       @tracker = AllureCucumber::FeatureTracker.tracker
       options = {:file => file, :title => title}
-      options.merge!(:step => @tracker.step_name) if attach_to_step
+      options.merge!(:step_id => @tracker.step_id) if attach_to_step
       if @tracker.scenario_name
         AllureRubyAdaptorApi::Builder.add_attachment(@tracker.feature_name, @tracker.scenario_name, options)
       else

lib/allure-cucumber/version.rb
@@ -1,5 +1,5 @@
 module AllureCucumber
   module Version
-    STRING = '0.6.1'
+    STRING = '0.6.2'
   end
 end