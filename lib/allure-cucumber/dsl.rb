5d4f49d473b642c37a03a8bd0db5f4ab78e63687
Pavel Strongin <pavel.s@circles.asia>
2018-07-30 16:15:35 +0800

    Cucumber 3 support!

--- lib/allure-cucumber/dsl.rb
+++ lib/allure-cucumber/dsl.rb
@@ -4,11 +4,11 @@ module AllureCucumber
     def attach_file(title, file, attach_to_step=true)
       @tracker = AllureCucumber::FeatureTracker.tracker
       options = {:file => file, :title => title}
-      options.merge!(:step => @tracker.step_id) if attach_to_step
+      options.merge!(:step_id => @tracker.step_id) if attach_to_step
       if @tracker.scenario_name
         AllureRubyAdaptorApi::Builder.add_attachment(@tracker.feature_name, @tracker.scenario_name, options)
       else
-        # TODO: This is possible for background steps.  
+        # TODO: This is possible for background steps.
         puts "Cannot attach #{title} to step #{@tracker.step_name} as scenario name is undefined"
       end
     end
