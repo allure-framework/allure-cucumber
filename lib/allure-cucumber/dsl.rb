e756baf15aac0e988e3c614af45feb25d354a743
Andrejs Kalnačs <andrejska@users.noreply.github.com>
2018-10-30 12:45:16 +0200

    add possibility to specify mime-type for attachment (via #74)

--- lib/allure-cucumber/dsl.rb
+++ lib/allure-cucumber/dsl.rb
@@ -1,9 +1,9 @@
 module AllureCucumber
   module DSL
 
-    def attach_file(title, file, attach_to_step=true)
+    def attach_file(title, file, attach_to_step=true, mime_type=nil)
       @tracker = AllureCucumber::FeatureTracker.tracker
-      options = {:file => file, :title => title}
+      options = {:file => file, :title => title, :mime_type => mime_type}
       options.merge!(:step_id => @tracker.step_id) if attach_to_step
       if @tracker.scenario_name
         AllureRubyAdaptorApi::Builder.add_attachment(@tracker.feature_name, @tracker.scenario_name, options)
